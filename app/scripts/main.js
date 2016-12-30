dc155a36efe760e4dcf6713b1953a4e478cc2a90
Eleazar Araujo <eleazar.araujo@wetransform.to>
2016-12-30 15:21:46 -0400

    Fix smooth scrolling error. Fix responsive styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('\'Allo \'Allo!');
+$( document ).ready(function() {
+  console.log( 'ready!' );
+
+  $('.lazy').slick({
+    lazyLoad: 'ondemand',
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+
+
+  var $root = $('html, body');
+  $('a').click(function() {
+    if( $(this).attr('data-target')=='#myCarousel') return;
+    var href = $.attr(this, 'href');
+    $root.animate({
+      scrollTop: $(href).offset().top - 110
+    }, 500, function () {
+      window.location.hash = href;
+    });
+    return false;
+
+  });
+
+});
